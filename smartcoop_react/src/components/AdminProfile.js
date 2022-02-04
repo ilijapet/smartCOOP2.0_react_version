f3013a381a9718e32cfa11cc0c9bafa954126d80
ilija <ilijapet@protonmail.com>
2022-02-04 22:43:55 +0100

    I add pause and unpaused function to admin profile.

--- smartcoop_react/src/components/AdminProfile.js
+++ smartcoop_react/src/components/AdminProfile.js
@@ -12,9 +12,13 @@ import { useEthers } from "@usedapp/core";
 const Admin = () => {
   const { ethereum } = window;
   const [buttonText, setButtonText] = useState("Withdraw all funds");
+  const [pauseButtonText, setPauseButtonText] = useState("Pause");
+  const [playButtonText, setPlayButtonText] = useState("Play");
 
   const [account, setAccount] = useState();
   const [newWidth, setNewWidth] = useState("0%");
+  const [newWidthPause, setNewWidthPause] = useState("0%");
+  const [newWidthPlay, setNewWidthPlay] = useState("0%");
   const { chainId } = useEthers();
 
   const getAccount = async () => {
@@ -40,6 +44,44 @@ const Admin = () => {
       });
   };
 
+  const pause = async () => {
+    const accounts = await ethereum.request({ method: "eth_accounts" });
+    const account = accounts[0];
+
+    var done = coopContract.methods
+      .pause()
+      .send({ from: account }, async function (error, transactionHash) {
+        if (error) {
+          console.log(error);
+        } else {
+          await progressButton(done, setNewWidthPause);
+          setPauseButtonText("Paused");
+          await sleep(4000);
+          setNewWidth("0%");
+          setPlayButtonText("Play");
+        }
+      });
+  };
+
+  const play = async () => {
+    const accounts = await ethereum.request({ method: "eth_accounts" });
+    const account = accounts[0];
+
+    var done = coopContract.methods
+      .unpause()
+      .send({ from: account }, async function (error, transactionHash) {
+        if (error) {
+          console.log(error);
+        } else {
+          await progressButton(done, setNewWidthPlay);
+          setPlayButtonText("Contract is again on");
+          await sleep(4000);
+          setNewWidth("0%");
+          setPauseButtonText("Pause");
+        }
+      });
+  };
+
   useEffect(() => {
     getAccount();
   });
@@ -53,12 +95,29 @@ const Admin = () => {
         <p> Currently on COOPaccount ETH: </p>
         <p> Currently rasperies in SmartCOOP warehuse </p>
         <p> Payed in COOP tokens to cooperants </p>
+
         <button id="btn__withdraw" onClick={withdraw}>
           <div
             className="button__progress_withdraw"
             style={{ width: newWidth }}
           ></div>
           {buttonText}
+        </button>
+
+        <button id="btn__withdraw" onClick={pause}>
+          <div
+            className="button__progress_withdraw"
+            style={{ width: newWidthPause }}
+          ></div>
+          {pauseButtonText}
+        </button>
+
+        <button id="btn__withdraw" onClick={play}>
+          <div
+            className="button__progress_withdraw"
+            style={{ width: newWidthPlay }}
+          ></div>
+          {playButtonText}
         </button>
       </section>
     </>
